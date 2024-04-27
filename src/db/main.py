a5a8446247e81a14eee96c1c905514154a5b3ae0
Maks-Siglov <gmb333x2@gmail.com>
2024-04-27 10:17:30 +0300

    User model

--- src/db/main.py
+++ src/db/main.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     async_sessionmaker,
-    create_async_engine,
+    create_async_engine
 )
 
 
---
+++ src/models/user.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+
+
+class User(Base):
+    __abstract__ = True
+
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+
+    is_active: Mapped[bool] = mapped_column(default=True)
+
+    created_at: Mapped[datetime] = mapped_column(default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), onupdate=func.now()
+    )
