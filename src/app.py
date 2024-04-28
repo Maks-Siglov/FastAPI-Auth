d4c306da9af26b36f8bfdf9ed94a1afab5fcda4f
Maks-Siglov <gmb333x2@gmail.com>
2024-04-28 22:48:11 +0300

    uvloop connected

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import asyncio
+import uvloop
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -15,6 +17,7 @@ async def lifespan(my_app: FastAPI):
     yield
     await close_dbs()
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 app = FastAPI(lifespan=lifespan)
 
