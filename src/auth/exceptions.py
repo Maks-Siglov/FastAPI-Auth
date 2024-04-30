58e6ae913aa40eda05675f2a9eb94e6a2b356984
Maks-Siglov <gmb333x2@gmail.com>
2024-04-30 19:56:41 +0300

    Auth exceptions headers added

--- src/auth/exceptions.py
+++ src/auth/exceptions.py
@@ -23,27 +23,32 @@ repeat_email_exception = HTTPException(
 invalid_token_exception = HTTPException(
     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
     detail="Invalid token",
+    headers={"WWW-Authenticate": "Bearer"},
 )
 
 
 invalid_token_credential_exception = HTTPException(
     status_code=status.HTTP_403_FORBIDDEN,
     detail="Invalid token credentials",
+    headers={"WWW-Authenticate": "Bearer"},
 )
 
 
 invalid_token_type_exception = HTTPException(
     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
     detail="Invalid token type",
+    headers={"WWW-Authenticate": "Bearer"},
 )
 
 revoked_token_error = HTTPException(
     status_code=status.HTTP_403_FORBIDDEN,
     detail="Token has been revoked",
+    headers={"WWW-Authenticate": "Bearer"},
 )
 
 
 unable_decode_jwt_exception = HTTPException(
     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
     detail="Unable to decode JWT",
+    headers={"WWW-Authenticate": "Bearer"},
 )
