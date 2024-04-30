1c3459e9eb52fa874c4cbbdfa675dd2e929a1afa
Maks-Siglov <gmb333x2@gmail.com>
2024-04-30 22:18:43 +0300

    Test invalid signup

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import pytest
 
-from src.models import User
+from src.core.settings import settings
 from src.app import app
-from src.db.main import get_session, set_session_pool, close_dbs
+from src.db.main import close_dbs, get_session, set_session_pool, session_pools
+from src.models import User
 
 
 @pytest.fixture(scope="session")
@@ -16,20 +17,20 @@ def test_client():
 
 @pytest.fixture(scope="session")
 async def db_connection():
-    await set_session_pool()
+    await set_session_pool(settings.db.test_postgres_url)
     yield
     await close_dbs()
 
 
 @pytest.fixture
 async def get_db_session() -> AsyncSession:
-    yield await get_session()
+    session = await get_session(settings.db.test_postgres_url)
+    yield session
+    await session.close()
 
 
 @pytest.fixture(scope="session")
 async def test_user(get_db_session: AsyncSession):
-    test_user = User(
-        email="test_email@gmail.com", password="Test_password22"
-    )
+    test_user = User(email="test_email@gmail.com", password="Test_password22")
     get_db_session.add(test_user)
     await get_db_session.commit()
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,12 +1,30 @@
+import pytest
 from starlette.testclient import TestClient
 
 
 def test_read_main(test_client: TestClient):
     login_post_data = {
         "email": "test_email@gmail.com",
-        "password": "Test_password22"
+        "password": "Test_password22",
     }
     response = test_client.post("/auth/login/", json=login_post_data)
 
     assert response.status_code == 401
     assert response.json()["detail"] == "Invalid user credentials"
+
+
+INVALID_SIGNUP_DATA = [
+    ({"email": "string", "password": "Test_password22"}, 422),
+    ({"email": "test_email2@gmail.com", "password": "no_uppercase2"}, 422),
+    ({"email": "test_email2@gmail.com", "password": "NO_LOVERCASE2"}, 422),
+    ({"email": "test_email2@gmail.com", "password": "No2symbol"}, 422),
+    ({"email": "test_email2@gmail.com", "password": "No_digit"}, 422),
+]
+
+
+@pytest.mark.parametrize("login_data, expected_code", INVALID_SIGNUP_DATA)
+def test_invalid_signup(
+    test_client: TestClient, login_data: dict[str, str], expected_code: int
+):
+    response = test_client.post("/auth/signup/", json=login_data)
+    assert response.status_code == expected_code
