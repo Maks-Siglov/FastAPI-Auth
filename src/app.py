cdefb4cb1d209268e24706fa381fc11b5a5eadde
Maks-Siglov <gmb333x2@gmail.com>
2024-05-01 19:55:46 +0300

    http_exception_handler provided

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 import asyncio
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 import uvicorn
 import uvloop
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from auth.views import router as auth_router
 from core.settings import settings
@@ -23,6 +25,16 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(
+    request: Request, exc: HTTPException
+) -> JSONResponse:
+    return JSONResponse(
+        status_code=exc.status_code, content={"detail": exc.detail}
+    )
+
 
 if __name__ == "__main__":
     uvicorn.run(
